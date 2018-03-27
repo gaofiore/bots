4adeedc7bda1f6c24ad7130c2b404dc8b76c0acf
Gabriel Fiorenzano <gfiorenzano@CalgaryChamber.com>
2018-03-27 15:40:24 -0600

    last changes

--- MembershipBot/index.js
+++ MembershipBot/index.js
@@ -1,37 +1,44 @@
-const restify = require('restify');
-const builder = require('botbuilder');
-const cognitiveServices = require('botbuilder-cognitiveservices');
-require('dotenv').config()
+var restify = require('restify');
+var builder = require('botbuilder');
+var azure = require('botbuilder-azure'); 
+var cognitiveservices = require('botbuilder-cognitiveservices');
 
-const server = restify.createServer();
-server.listen(process.env.port || process.env.PORT || 3978,
-    function () {
-        console.log('%s listening to %s',server.name,server.url);
-    }
-);
+//=========================================================
+// Bot Setup
+//=========================================================
 
-const connector = new builder.ChatConnector({
-    appId: process.env.MicrosoftAppId,
-    appPassword: process.env.MicrosoftAppPassword
+// Setup Restify Server
+var server = restify.createServer();
+server.listen(process.env.port || process.env.PORT || 3978, function () {
+    console.log('%s listening to %s', server.name, server.url);
 });
 
+// Create chat bot
+var connector = new builder.ChatConnector({
+    appId: process.env.MICROSOFT_APP_ID,
+    appPassword: process.env.MICROSOFT_APP_PASSWORD
+});
 var bot = new builder.UniversalBot(connector);
+bot.set('storage', new builder.MemoryBotStorage());         // Register in-memory state storage
 server.post('/api/messages', connector.listen());
 
-const recognizer = new cognitiveServices.QnAMakerRecognizer({
-    knowledgeBaseId: process.env.KNOWLEDGE_BASE_ID,
-    subscriptionKey: process.env.SUBSCRIPTION_KEY,
-    top: 3
-});
+//=========================================================
+// Bots Dialogs
+//=========================================================
 
-var qnaMakerTools = new cognitiveServices.QnAMakerTools();
+var recognizer = new cognitiveservices.QnAMakerRecognizer({
+    knowledgeBaseId: '8af9d92f-8354-452b-965d-82a1d86da5d3',
+    subscriptionKey: '039711dc2c6d483487072081ed00fd68',
+    top: 5});
+
+var qnaMakerTools = new cognitiveservices.QnAMakerTools();
 bot.library(qnaMakerTools.createLibrary());
 
-const qnAMakerDialog = new cognitiveServices.QnAMakerDialog({
+var basicQnAMakerDialog = new cognitiveservices.QnAMakerDialog({
     recognizers: [recognizer],
-    defaultMessage: "Sorry I don't understand the question",
-    qnaThreshold: 0.4,
+    defaultMessage: 'No match! Try changing the query terms!',
+    qnaThreshold: 0.5,
     feedbackLib: qnaMakerTools
 });
 
-bot.dialog('/', qnAMakerDialog);
+bot.dialog('/', basicQnAMakerDialog);
